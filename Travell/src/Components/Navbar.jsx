0b50eb617b58cf95c0f958a356729017e0f411ac
halwohamda <halwohamda@gmail.com>
2024-02-29 09:48:25 -0800

    done

--- Travell/src/Components/Navbar.jsx
+++ Travell/src/Components/Navbar.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import {Link} from 'react-scroll';
+import {AiOutlineClose} from 'react-icons';
+import {AiOutlineMenuClose} from 'react-icons';
 
 function Navbar() {
   return (
     <div>
         <div>
-            
+            <div>
+                
+           </div>
         </div>
     </div>
   )
