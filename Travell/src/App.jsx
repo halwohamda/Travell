6336cbadcfbca5b20796cf0a93658a4aa1b54593
halwohamda <halwohamda@gmail.com>
2024-02-29 18:11:58 -0800

    done

--- Travell/src/App.jsx
+++ Travell/src/App.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import Navbar from './Components/Navbar';
+import Home from './Components/Home';
+import Howto from './Components/Howto';
 
 
 const App = () => {
   return (
     <div>
-
       <Navbar/>
       
+      <main>
+        <div className="home">
+          <Home/>
+        </div>
+        <div className="howto">
+          <Howto/>
+        </div>
+      </main>
     </div>
 
   )
