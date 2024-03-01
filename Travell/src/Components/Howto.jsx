78b0360c487ab1dc5ad6729cf2f8ba6e4f3bf616
halwohamda <halwohamda@gmail.com>
2024-02-29 18:17:39 -0800

    done

--- Travell/src/Components/Howto.jsx
+++ Travell/src/Components/Howto.jsx
@@ -5,8 +5,14 @@ const Howto = () => {
   return (
     <div>
       <div>
+        <h2 className='text-purple-500 text-center text-4xl font-bold'>Features</h2>
+        
+      </div>
+      <div>
         <span><IoBed /></span>
       </div>
+      <h2>Hotels bed</h2>
+      <p>Lorem ipsum dolor sit amet <br /> consectetur adipisicing elit. Quaer</p>
 
 
 
