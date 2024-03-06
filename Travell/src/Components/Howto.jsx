9951944fafcc21dbde3d7e76dc1dbf4cd18c36aa
halwohamda <halwohamda@gmail.com>
2024-03-05 17:57:04 -0800

    done

--- Travell/src/Components/Howto.jsx
+++ Travell/src/Components/Howto.jsx
@@ -33,13 +33,13 @@ const Howto = () => {
         <h2 className='text-purple-500 text-center text-4xl font-bold'>Features</h2>
         <h3 className='text-center font-semibold text-5xl'>Our Princeles Feater</h3>
         <p className='text-center'>Lorem ipsum dolor sit amet consectetur <br /> adipisicing elit. Quidem omnis, nam labori</p>
-{/*         
+        
       </div>
      <div className='flex md:flex-row flex-col w-full items-center justify-between md:gap-0 gap-5 '>
          {Featureslist.map((feature)=>{
           return <div className='bg-white py-5 px-12 flex flex-col items-center justify-center gap-5 rounded-[10px] transition-all  duration-300 cursor-pointer hover:scale-[1.05]'  style={{boxShadow: "0 0 40px 5px rgb(0 0 0 5% )" }} key={feature.title}>
             {feature.icons}
-            <span className='font-semibold text-gray-700 text-[21px]'> */}
+            <span className='font-semibold text-gray-700 text-[21px]'>
               {feature.title}
             </span>
             <span className=' text-center max-w-[360px] loading-7'> {feature.Description}</span>
