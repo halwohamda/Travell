8f5c2cecdc4bab2babc93198c1d1fe4b67204acf
halwohamda <halwohamda@gmail.com>
2024-03-05 19:09:25 -0800

    done

--- Travell/src/Components/Newspaper.jsx
+++ Travell/src/Components/Newspaper.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+// import React from 'react'
 
-const Newspaper = () => {
-  return (
-    <div>Newspaper</div>
-  )
-}
+// const Newspaper = () => {
+//   return (
+//     <div>Newspaper</div>
+//   )
+// }
 
-export default Newspaper
+// export default Newspaper
