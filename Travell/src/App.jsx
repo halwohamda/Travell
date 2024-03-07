964a73667bfdf9c1232e27a03c6aa1c26cc5a288
halwohamda <halwohamda@gmail.com>
2024-03-07 10:47:07 -0800

    done

--- Travell/src/App.jsx
+++ Travell/src/App.jsx
@@ -4,6 +4,7 @@ import Home from './Components/Home';
 import Howto from './Components/Howto';
 import Service from './Components/Service';
 import Blog from './Components/Blog';
+import Contact from './Components/Contact';
 
 
 const App = () => {
@@ -23,6 +24,9 @@ const App = () => {
         </div>
         <div className="blog">
           <Blog/>
+        </div>
+        <div className="contact">
+          <Contact/>
         </div>
       </main>
     </div>
