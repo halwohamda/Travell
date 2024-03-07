8ef3663fec5aef6f872141c9e7fb360de54485b9
halwohamda <halwohamda@gmail.com>
2024-03-07 11:45:50 -0800

    done

--- Travell/src/Components/Contact.jsx
+++ Travell/src/Components/Contact.jsx
@@ -10,9 +10,9 @@ const Contact = () => {
    It's Time to Enjoy  <br />
       Your Freedom!
    </span>
-<div className="flex md:flex-row flex-col gap-10 mb-2 md:mb-0 ">
+<div className="flex md:flex-row flex-row gap-10 mb-2 md:mb-0 ">
   <input type="text" placeholder='Email Address'
-  className=' md:w-[400px] w-[200px] border border-slate-400 outline-none px-3 rounded-[10px] py-3'
+  className=' md:w-[400px] w-[300px] border border-slate-400 outline-none px-3 rounded-[10px] py-3'
   />
   <Button title='submit'/>
   
