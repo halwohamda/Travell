db016c6d2dce03bb39a7280e8430403641d6248d
halwohamda <halwohamda@gmail.com>
2024-03-07 10:58:03 -0800

    done

--- Travell/src/Components/Contact.jsx
+++ Travell/src/Components/Contact.jsx
@@ -5,7 +5,6 @@ const Contact = () => {
   return (
     <div className='w-full md:w-[400px] md:h-[480px] my-20 bg-fixed bg-cover relative z-[1] '> 
       <div className='mt-20'>
-        <img src={sky} alt="" className='w-full h-40'  />
       </div>
     </div>
   )
