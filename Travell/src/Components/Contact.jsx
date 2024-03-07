444dc5aab5c465644120098a6c8624f6a8e7ac27
halwohamda <halwohamda@gmail.com>
2024-03-07 11:20:28 -0800

    done

--- Travell/src/Components/Contact.jsx
+++ Travell/src/Components/Contact.jsx
@@ -4,7 +4,7 @@ import sky from '../Private/sky.jfif';
 const Contact = () => {
   return (
     <div className=' w-full md:h-[400px] h-[460px] my-20 bg-fixed bg-cover relative z-[1]   ' style={{backgroundImage:`url(${sky})`, backgroundPosition: "bottom"}}> 
-<div className="flex w-full h-full md:flex-row text-4xl items-center text-white text-bold justify-between px-10  ">
+<div className="flex w-full h-full md:flex-row text-4xl items-center text-white text-bold justify-between px-10 banner z-[3] md:px-32 ">
       It's Time to Enjoy <br />
       Your Freedom
 
