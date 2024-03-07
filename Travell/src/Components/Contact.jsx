de90656eb575c50657db98c3c5a28b5d5f6a847f
halwohamda <halwohamda@gmail.com>
2024-03-07 11:47:00 -0800

    done

--- Travell/src/Components/Contact.jsx
+++ Travell/src/Components/Contact.jsx
@@ -5,12 +5,12 @@ import Button from '../Layout/Button';
 const Contact = () => {
   return (
     <div className=' w-full md:h-[400px] h-[460px] my-20 bg-fixed bg-cover relative z-[1]   ' style={{backgroundImage:`url(${sky})`, backgroundPosition: "bottom"}}> 
-<div className="  w-full h-full flex flex-col md:flex-row items-center justify-between px-10 banner z-[3] md:py-0 py-5 text-center ">
+<div className="  w-full h-full flex flex-row md:flex-col items-center justify-between px-10 banner z-[3] md:py-0 py-5 text-center ">
    <span className='text-white md:text-[55px] text-[40px] font-semibold'>
    It's Time to Enjoy  <br />
       Your Freedom!
    </span>
-<div className="flex md:flex-col flex-row gap-10 mb-2 md:mb-0 ">
+<div className="flex md:flex-row flex-col gap-10 mb-2 md:mb-0 ">
   <input type="text" placeholder='Email Address'
   className=' md:w-[400px] w-[300px] border border-slate-400 outline-none px-3 rounded-[10px] py-3'
   />
