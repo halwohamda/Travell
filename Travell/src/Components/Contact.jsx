8c96caac5ce576bdc6e946ee55725d33324a9454
halwohamda <halwohamda@gmail.com>
2024-03-07 10:55:58 -0800

    done

--- Travell/src/Components/Contact.jsx
+++ Travell/src/Components/Contact.jsx
@@ -3,7 +3,7 @@ import sky from '../Private/sky.jfif';
 
 const Contact = () => {
   return (
-    <div className='w-full m-auto md:flex-col flex  '> 
+    <div className='w-full md:w-[400px] md:h-[480px] '> 
       <div className='mt-20'>
         <img src={sky} alt="" className='w-full h-40'  />
       </div>
