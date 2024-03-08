896f998f799edb7aadc32868809818c806fdbc81
halwohamda <halwohamda@gmail.com>
2024-03-08 12:32:32 -0800

    done

--- Travell/src/Components/About.jsx
+++ Travell/src/Components/About.jsx
@@ -1,28 +1,28 @@
-import React from 'react'
+// import React from 'react'
 
-const About = () => {
-  return (
-    <div className='w-full flex md:flex-row flex-col md:gap-0 gap-8 items-center justify-between px-8 py-10 border-t border-slate-300 border-dashed '>
-      <span className="font-medium text-slate-400 ">
-        copyright & 2024 halwo travell All right reserved.
-      </span>
-      <div className="flex items-center gap-8">
-        <a href="#" className='font-medium whitespace-nowrap md:text-[15px] text-[10.5px] text-gray-700 hover:text-purple-500'>
-          Terms and Conditions 
-        </a>
-        <a href="#" className='font-medium whitespace-nowrap md:text-[15px] text-[10.5px] text-gray-700 hover:text-purple-500'>
-         long Terms contract 
-        </a>
-        <a href="#" className='font-medium whitespace-nowrap md:text-[15px] text-[10.5px] text-gray-700 hover:text-purple-500'>
-         copyright policy
-        </a>
-        <a href="#" className='font-medium whitespace-nowrap md:text-[15px] text-[10.5px] text-gray-700 hover:text-purple-500'>
-         customers support
-        </a>
+// const About = () => {
+//   return (
+//     <div className='w-full flex md:flex-row flex-col md:gap-0 gap-8 items-center justify-between px-8 py-10 border-t border-slate-300 border-dashed '>
+//       <span className="font-medium text-slate-400 ">
+//         copyright & 2024 halwo travell All right reserved.
+//       </span>
+//       <div className="flex items-center gap-8">
+//         <a href="#" className='font-medium whitespace-nowrap md:text-[15px] text-[10.5px] text-gray-700 hover:text-purple-500'>
+//           Terms and Conditions 
+//         </a>
+//         <a href="#" className='font-medium whitespace-nowrap md:text-[15px] text-[10.5px] text-gray-700 hover:text-purple-500'>
+//          long Terms contract 
+//         </a>
+//         <a href="#" className='font-medium whitespace-nowrap md:text-[15px] text-[10.5px] text-gray-700 hover:text-purple-500'>
+//          copyright policy
+//         </a>
+//         <a href="#" className='font-medium whitespace-nowrap md:text-[15px] text-[10.5px] text-gray-700 hover:text-purple-500'>
+//          customers support
+//         </a>
 
-      </div>
-    </div>
-  )
-}
-
-export default About
+//       </div>
+//     </div>
+//   )
+// }
+// 
+// export default About
