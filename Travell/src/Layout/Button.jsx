001fc4c32bd8eca1a2a9bf73ae356f748471ef10
halwohamda <halwohamda@gmail.com>
2024-03-07 17:28:05 -0800

    done

--- Travell/src/Layout/Button.jsx
+++ Travell/src/Layout/Button.jsx
@@ -1,13 +1,13 @@
-// import React from 'react'
+import React from 'react'
 
-// const Button = (props) => {
-//   return (
-//     <div>
-//            <button className='px-6 py-1 border-white bg-purple-700 hover:text-purple-700 transition-all  rounded-full border-2 text-white hover:bg-white  hover:border-purple-600  '>
-//                 {props.title}
-//         </button>
-//     </div>
-//   )
-// }
+const Button = (props) => {
+  return (
+    <div>
+           <button className='px-6 py-1 border-white bg-purple-700 hover:text-purple-700 transition-all  rounded-full border-2 text-white hover:bg-white  hover:border-purple-600  '>
+                {props.title}
+        </button>
+    </div>
+  )
+}
 
-// export default Button
+export default Button
