4b3449ffbdd588c1a008a69426069a6f8af2d0e9
halwohamda <halwohamda@gmail.com>
2024-03-08 09:48:58 -0800

    done

--- Travell/src/Components/Newspaper.jsx
+++ Travell/src/Components/Newspaper.jsx
@@ -1,18 +1,1 @@
-import React from 'react'
-import airpor from '../Private/news1.jfif'
 
-const Newspaper = () => {
-  return (
-    <div>
-
- <div>
-  <img src={airpor} alt="" />
- </div>
-
-
-
-    </div>
-  )
-}
-
-export default Newspaper
