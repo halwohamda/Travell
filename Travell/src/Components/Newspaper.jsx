fc6e606433f8f535430b13aacc9cdfe0af9f4451
halwohamda <halwohamda@gmail.com>
2024-03-08 10:47:49 -0800

    done

--- Travell/src/Components/Newspaper.jsx
+++ Travell/src/Components/Newspaper.jsx
@@ -34,7 +34,8 @@ const Newspaper = () => {
     <div className='flex w-full pb-20 md:px-[200px] px-8 flex-col gap-16 justify-center items-center'>
 
      <div className="flex flex-col w-full items-center">
-      <span className='font-semibold text-purple-700 text-4xl'>Our Blog</span>
+      <span className='font-semibold text-purple-700 '>Our Blog</span>
+      <span className='font-semibold text-slate-700 mt-4 text-4xl '>Our List Blog</span>
 
      </div>
 
