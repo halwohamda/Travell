3c62a10cd061f8d6770460c9e607998e3dcfe406
halwohamda <halwohamda@gmail.com>
2024-03-08 10:39:41 -0800

    done

--- Travell/src/Components/Newspaper.jsx
+++ Travell/src/Components/Newspaper.jsx
@@ -3,7 +3,28 @@ import React from 'react'
 const Newspaper = () => {
   const newslist = [
     {
-      
+      Image:"news1",
+      autother:"halwo adam",
+      date:"3/8/2024",
+      title:"Most Visited Contires in 3 Years",
+      Description:""
+
+    },
+    {
+      Image:"news1",
+      autother:"halwo adam",
+      date:"3/8/2024",
+      title:"Most Visited Contires in 3 Years",
+      Description:""
+
+    },
+    {
+      Image:"news1",
+      autother:"halwo adam",
+      date:"3/8/2024",
+      title:"Most Visited Contires in 3 Years",
+      Description:""
+
     }
   ]
   return (
