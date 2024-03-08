e7041a3eb0bc8f876f2f35f55d5ca4465685e7c9
halwohamda <halwohamda@gmail.com>
2024-03-08 10:35:50 -0800

    done

--- Travell/src/Components/Newspaper.jsx
+++ Travell/src/Components/Newspaper.jsx
@@ -1,34 +1,34 @@
-import React from 'react'
-import news1 from '../News/news1.jfif'
-import { CiUser } from "react-icons/ci";
-import { MdDateRange } from "react-icons/md";
+// import React from 'react'
+// import news1 from '../News/news1.jfif'
+// import { CiUser } from "react-icons/ci";
+// import { MdDateRange } from "react-icons/md";
 
 
-const Newspaper = () => {
-  return (
-    <div className='w-full h-full md:flex-row flex-col  text-center'>
-      <div className='flex-row'>
-        <span className='text-4xl text-purple-500 font-semiboldbold'>Our Blog</span> <br />
-        <h1 className='mt-3 text-4xl font-bold'>Our List Blog</h1> <br />
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum libero sequi</p> <br />
+// const Newspaper = () => {
+//   return (
+//     <div className='w-full h-full md:flex-row flex-col  text-center'>
+//       <div className='flex-row'>
+//         <span className='text-4xl text-purple-500 font-semiboldbold'>Our Blog</span> <br />
+//         <h1 className='mt-3 text-4xl font-bold'>Our List Blog</h1> <br />
+//         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum libero sequi</p> <br />
       
-      <div className="flex bg-white p-[10px] rounded-[10px] transition-all m-4  duration-300 cursor-pointer hover:scale-[1.05]"
-         style={{boxShadow:"0 0 40px 5px rgb(0 0 0 /5%) "}}
-       >
-       <img src={news1} alt="" />
-      </div>
-      <div className="flex  md:flex-row m-4 ">
-       <CiUser  /> <span>Halwo adm</span>
-       <MdDateRange /> <span>3/8/2024</span> 
-       <h1 className='mt-5 mr-[10px]'>Most visited Contiries in 3 years</h1> <br />
-       </div>
+//       <div className="flex bg-white p-[10px] rounded-[10px] transition-all m-4  duration-300 cursor-pointer hover:scale-[1.05]"
+//          style={{boxShadow:"0 0 40px 5px rgb(0 0 0 /5%) "}}
+//        >
+//        <img src={news1} alt="" />
+//       </div>
+//       <div className="flex  md:flex-row m-4 ">
+//        <CiUser  /> <span>Halwo adm</span>
+//        <MdDateRange /> <span>3/8/2024</span> 
+//        <h1 className='mt-5 mr-[10px]'>Most visited Contiries in 3 years</h1> <br />
+//        </div>
     
 
-</div>
+// </div>
 
 
-    </div>
-  )
-}
+//     </div>
+//   )
+// }
 
-export default Newspaper
+// export default Newspaper
