c2f23125f0dab70639ec608c101808abc0bb0741
halwohamda <halwohamda@gmail.com>
2024-03-08 10:31:47 -0800

    done

--- Travell/src/Components/Newspaper.jsx
+++ Travell/src/Components/Newspaper.jsx
@@ -11,18 +11,20 @@ const Newspaper = () => {
         <span className='text-4xl text-purple-500 font-semiboldbold'>Our Blog</span> <br />
         <h1 className='mt-3 text-4xl font-bold'>Our List Blog</h1> <br />
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum libero sequi</p> <br />
-      </div>
+      
       <div className="flex bg-white p-[10px] rounded-[10px] transition-all m-4  duration-300 cursor-pointer hover:scale-[1.05]"
          style={{boxShadow:"0 0 40px 5px rgb(0 0 0 /5%) "}}
        >
        <img src={news1} alt="" />
       </div>
-      <div className="mt-5">
+      <div className="flex  md:flex-row ">
        <CiUser  /> <span>Halwo adm</span>
-       <MdDateRange /> <span>3/8/2024</span>
+       <MdDateRange /> <span>3/8/2024</span> 
+     
        </div>
+       <h1>Most visited Contiries in 3 years</h1> <br />
 
-
+</div>
 
 
     </div>
