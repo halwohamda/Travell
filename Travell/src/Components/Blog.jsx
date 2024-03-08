3c86ce6b7194061d9f88780ca2ed781004044970
halwohamda <halwohamda@gmail.com>
2024-03-08 09:32:39 -0800

    done

--- Travell/src/Components/Blog.jsx
+++ Travell/src/Components/Blog.jsx
@@ -69,7 +69,7 @@ const Blog = () => {
       <div className="w-[92%] mt-5 mx-auto ">
         <div className="w-full px-4">
        <span className='flex'>
-       <Button title='BookNow' type="submit"/>    
+        <input type="submit"/>    <Button title='BookNow' type="submit"/>    
        </span>
         </div>
 
