352b99cf19c8839dd4825c01c235e9aadf870da5
halwohamda <halwohamda@gmail.com>
2024-03-08 10:15:46 -0800

    done

--- Travell/src/Components/Newspaper.jsx
+++ Travell/src/Components/Newspaper.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Newspaper = () => {
   return (
     <div className='w-full h-full md:flex-row flex-col  text-center'>
-      <div className="text-4xl text-purple-500 font-semiboldbold">
-        <span>Our Blog</span> <br />
-        <h1>Our List Blog</h1> <br />
+      <div>
+        <span className='text-4xl text-purple-500 font-semiboldbold'>Our Blog</span> <br />
+        <h1 className='mt-3 text-4xl font-bold'>Our List Blog</h1> <br />
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum libero sequi</p> <br />
       </div>
 
