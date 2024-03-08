a66d5fe159cf9b1a3ccb4a43044f324733d983b1
halwohamda <halwohamda@gmail.com>
2024-03-08 10:11:45 -0800

    done

--- Travell/src/Components/Newspaper.jsx
+++ Travell/src/Components/Newspaper.jsx
@@ -4,11 +4,11 @@ const Newspaper = () => {
   return (
     <div>
       <div className="flex">
-        <span>Our Blog</span>
-        <h1>Our List Blog</h1>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum libero sequi</p>
+        <span>Our Blog</span> <br />
+        {/* <h1>Our List Blog</h1> <br />
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum libero sequi</p> <br /> */}
       </div>
-      
+
 
 
 
