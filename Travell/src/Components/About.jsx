b4eec2f31133c4388431b3f5bece81f8e6e36a6a
halwohamda <halwohamda@gmail.com>
2024-03-08 11:52:49 -0800

    done

--- Travell/src/Components/About.jsx
+++ Travell/src/Components/About.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div className='W-full flex md:flex '>
+    <div className='W-full flex md:flex-row flex-col md:gap-0 gap-8 items-center justify-between px-8 py-10 border-t border-slate-800 '>
 
     </div>
   )
