424162b6190a498e802a0ef5ee374c5caea468f3
halwohamda <halwohamda@gmail.com>
2024-03-08 12:04:03 -0800

    done

--- Travell/src/Components/About.jsx
+++ Travell/src/Components/About.jsx
@@ -7,7 +7,15 @@ const About = () => {
         copyright & 2024 halwo travell All right reserved.
       </span>
       <div className="flex items-center gap-8">
-        <a href="#" className='font-medium whitespace-nowrap md:text-[15px] text-[10.5px] text-gray-700 hover:text-purple-500'></a>
+        <a href="#" className='font-medium whitespace-nowrap md:text-[15px] text-[10.5px] text-gray-700 hover:text-purple-500'>
+          Terms and Conditions 
+        </a>
+        <a href="#" className='font-medium whitespace-nowrap md:text-[15px] text-[10.5px] text-gray-700 hover:text-purple-500'>
+         long Terms contract 
+        </a>
+        <a href="#" className='font-medium whitespace-nowrap md:text-[15px] text-[10.5px] text-gray-700 hover:text-purple-500'>
+         copyright policy
+        </a>
 
       </div>
     </div>
